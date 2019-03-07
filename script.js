658c8c9ed85e69d873e303a184c7a8beafd028f3
argenis0409 <argenis04_09@icloud.com>
2019-03-06 23:18:27 -0800

    updated the script file and deleted the public file

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello world!!!')
+//Variables mutation and type coercion
+
+var firstName = 'Jhon';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+age = 'twenty eight';
+job = 'drivewr';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
