9feada439568df921d94366ea0b24937d7e7c361
argenis0409 <argenis04_09@icloud.com>
2019-03-08 12:17:53 -0800

    examples of objects and properties

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+
+
+
+/********* Objects and properties **********/
+
+var jhon = {
+    firstName: 'jhon',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    Job: 'teacher',
+    isMarried: false
+};
+
+console.log(jhon.firstName);
+console.log(jhon['lastName']);
+var x = 'birthYear';
+console.log(jhon[x]);
+
+jhon.job = 'designer';
+jhon['isMarried'] = true;
+console.log(jhon);
+
+// new Object syntax
+var Jane = new Object();
+Jane.firstName = 'Jane';
+Jane.birthYear = 1969;
+Jane['lastName'] = 'Smith';
+console.log(Jane);
